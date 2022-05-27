e0c2ee1a5655cc41181522216bcf4f77e7fa4594
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-05-27 15:39:05 +0000

    Add post model to blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Post(models.Model):
+    """Main post model"""
+    title = models.CharField(max_length=150)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    excerpt = models.CharField(max_length=200)
+    content = models.TextField()
+    image = CloudinaryField('image', default='placeholder', blank=True)
+    created_on = models.DateTimeField(default=timezone.now)
+    likes = models.ManyToManyField(User, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
