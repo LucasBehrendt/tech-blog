185120229a237627d46bdd3b659851073c4e0543
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-05-27 20:24:11 +0000

    Register models on admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post table on admin page"""
+    list_filter = ('created_on',)
+    list_display = ('author', 'title', 'excerpt', 'created_on')
+    search_fields = ['title', 'excerpt', 'author__username']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Comment table on admin page"""
+    list_filter = ('created_on',)
+    list_display = ('author', 'body', 'post', 'created_on')
+    search_fields = ['author__username', 'body']
