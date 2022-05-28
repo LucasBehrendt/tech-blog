5cd201f407202f8e082edd5f2858d04ee48572f9
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-05-28 14:58:37 +0000

    Add home view & setup url routing

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+
+class PostList(ListView):
+    model = Post
+    ordering = ['-created_on']
+    paginate_by = 2
