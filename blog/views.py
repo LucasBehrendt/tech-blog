f4304f617ad98d2216a3c4a76eea2a56803d4a3f
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-05-31 19:32:26 +0000

    Create view, url & template for post-detail.html

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.db.models import Count
 from .models import Post
 
@@ -15,3 +15,7 @@ class PostList(ListView):
     ordering = ['-created_on']
     paginate_by = 2
     queryset = Post.objects.annotate(number_of_comments=Count('comment'))
+
+
+class PostDetail(DetailView):
+    model = Post
