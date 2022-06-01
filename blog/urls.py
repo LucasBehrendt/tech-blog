24604e2740c94ad0ff3dc56ebfaffaedf78d0e85
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-06-01 19:49:11 +0000

    Create users app & add to INSTALLED_APPS

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('post/<int:pk>/', views.PostDetail.as_view(), name='post-detail'),
+    path('post/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
     path('blog/about/', views.about, name='about'),
 ]
