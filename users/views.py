4ebb4c6283020767f4aea0647600cec7ce7327f4
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-06-01 20:28:59 +0000

    Create view, url & template for register.html

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    """Main view when registering new users"""
+    form = UserCreationForm()
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
