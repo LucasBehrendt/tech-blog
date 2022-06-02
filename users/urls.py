c826a6b0e545232031818668977cee76ea1529dd
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-06-02 19:17:15 +0000

    Create sign in url & template

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
+    path('signin/', auth_views.LoginView.as_view(
+        template_name='users/signin.html'), name='signin'),
 ]
