65ec3879acfb21bdd30a45ea5ce53cddf2015a33
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-06-02 20:03:49 +0000

    Style text links & wire up signin/signout form

--- tech_blog/settings.py
+++ tech_blog/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'crispy_forms',
 ]
 
+LOGIN_REDIRECT_URL = '/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
