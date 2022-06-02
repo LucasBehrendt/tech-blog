a20b9dc4f2d80a1415df8450f35caba78bf66c64
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-06-02 12:40:42 +0000

    Add POST handling to register view

--- users/views.py
+++ users/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 def register(request):
     """Main view when registering new users"""
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created, \
+                you are now logged in as {username}!')
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
     context = {'form': form}
     return render(request, 'users/register.html', context)
