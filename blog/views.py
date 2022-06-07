493f2248638be288374af566eff8ea4bb4a7b1bc
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-06-07 21:02:55 +0000

    Add form handling to PostCreate view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.views import generic
 from django.db.models import Count
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from .models import Post
 
 
@@ -13,7 +15,7 @@ class PostList(generic.ListView):
     """Main post feed view"""
     model = Post
     ordering = ['-created_on']
-    paginate_by = 2
+    paginate_by = 5
     queryset = Post.objects.annotate(number_of_comments=Count('comment'))
 
 
@@ -22,7 +24,13 @@ class PostDetail(generic.DetailView):
     model = Post
 
 
-class PostCreate(generic.CreateView):
+class PostCreate(LoginRequiredMixin, SuccessMessageMixin, generic.CreateView):
     """View for creating post"""
     model = Post
     fields = ['title', 'excerpt', 'content', 'image']
+    success_message = 'You post was submitted successfully!'
+    success_url = '/post/{id}'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
