c26cc213265a9204e1a73b627f7340231472ef8f
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-06-15 13:57:34 +0000

    Create tests for users app form

---
+++ users/tests/test_forms.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from users.forms import UserRegisterForm
+
+
+class TestForm(TestCase):
+    """Tests for blog app form"""
+
+    def test_user_register_form(self):
+        form = UserRegisterForm(
+            data={
+                'username': 'testUser',
+                'email': 'test@email.com',
+                'password1': 'testpassword',
+                'password2': 'testpassword',
+            }
+        )
+
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.data['username'], 'testUser')
+        self.assertEqual(form.data['email'], 'test@email.com')
+        self.assertEqual(form.data['password1'], 'testpassword')
+        self.assertEqual(form.data['password2'], 'testpassword')
+
+    def test_user_register_form_invalid_data(self):
+        form = UserRegisterForm(
+            data={
+                'username': 'testUser',
+                'email': 'test@email.com',
+                'password1': 'testpass',
+                'password2': 'testpassword',
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['password2'][0],
+                         'The two password fields didn’t match.')
