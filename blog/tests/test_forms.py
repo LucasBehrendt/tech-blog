42503f50cdfb9d263de15a651d3ed0bb106d7865
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-06-15 12:34:50 +0000

    Create tests for blog app form

--- blog/tests/test_forms.py
+++ blog/tests/test_forms.py
@@ -1,1 +1,18 @@
 from django.test import TestCase
+from blog.forms import CommentForm
+
+
+class TestForm(TestCase):
+    """Tests for blog app form"""
+
+    def test_comment_form(self):
+        form = CommentForm(data={'body': 'testComment'})
+
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.data['body'], 'testComment')
+
+    def test_comment_form_invalid_data(self):
+        form = CommentForm(data={'body': ''})
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['body'][0], 'This field is required.')
