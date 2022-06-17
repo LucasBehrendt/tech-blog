7e1dcdf1ad8952495620ebddb4e8048c1092cdcb
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-06-17 17:42:53 +0000

    Fix issue with duplicate usernames

--- users/forms.py
+++ users/forms.py
@@ -11,3 +11,12 @@ class UserRegisterForm(UserCreationForm):
         """Definition of fields used in form"""
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def clean(self):
+        cleaned_data = super(UserRegisterForm, self).clean()
+        username = cleaned_data.get('username')
+        if username and User.objects.filter(
+                username__iexact=username).exists():
+            self.add_error('username',
+                           'A user with that username already exists.')
+        return cleaned_data
